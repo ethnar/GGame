6da8f8be7056ae7e55567a92c36bf2e432a459c5
Arek Bisaga <ethnar.dev@gmail.com>
2018-05-24 18:27:11 +0100

    Updated hide icon

--- class/items/hide.js
+++ class/items/hide.js
@@ -4,5 +4,9 @@ class Hide extends Item {
     static entityName() {
         return 'Hide';
     }
+
+    static icon() {
+        return '/iconpack/leather/lz_b_03.png';
+    }
 }
 module.exports = global.Hide = Hide;
